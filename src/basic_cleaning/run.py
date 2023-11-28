0937ccb46de5e9968afb9a28959225243156d09e
Your-Full-Name <alfonso.ridolfo@gmail.com>
2023-11-28 21:29:15 +0100

    fix: out of boundaries coordinates cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -41,6 +41,12 @@ def go(args):
     # Convert last_review to datetime
     logger.info("converting last_review to datetime...")   
     df["last_review"] = pd.to_datetime(df["last_review"])
+    
+    # drop rows where coordinates are out of boundaries
+    logger.info("dropping records with out of boundaries coordinates...")   
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     df.to_csv("clean_sample.csv", index="id")
 
     # upload output to W&B
